70f43e88904099c424bb6c4e27e150e40f3586a7
Porrselvi <57982864+Porrselvi@users.noreply.github.com>
2020-02-27 14:10:38 -0500

    Python Prog- Lists and sets

---
+++ list and set.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+
+
+
+# In[32]:
+
+
+
+
+
+# In[21]:
+
+
+pwd
+
+
+# In[22]:
+
+
+mpg = pd.read_csv('mpg.csv')
+
+
+# In[23]:
+
+
+mpg.head(10)
+
+
+# In[30]:
+
+
+listofv = {1,2,3,4}
+
+
+# In[31]:
+
+
+listofv[2]
+
+
+# In[37]:
+
+
+val={2,3,4,5}
+
+
+# In[38]:
+
+
+type(val)
+
+
+# In[39]:
+
+
+lst
+
+
+# In[40]:
+
+
+type(lst)
+
+
+# In[41]:
+
+
+list = [1,2,333,4,4]
+
+
+# In[43]:
+
+
+list
+
+
+# In[44]:
+
+
+set_1 = {1,2,3,5}
+
+
+# In[45]:
+
+
+set_1
+
+
+# In[46]:
+
+
+set_1.add('5')
+
+
+# In[47]:
+
+
+set_1
+
+
+# In[48]:
+
+
+set_1.add(5)
+
+
+# In[49]:
+
+
+set_1
+
+
+# In[51]:
+
+
+# append and extend will work only in list() function. In the set we can add value using add().
+#remove()-set
+#"5" in set_1
+
+
+# In[52]:
+
+
+5 in set_1
+
+
+# In[53]:
+
+
+0 in set_1
+
+
+# In[ ]:
+
+
+# & operation to join the 
+
