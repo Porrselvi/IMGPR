e0166bf958857476c9176c2b78f76951d6f770e5
Porrselvi <57982864+Porrselvi@users.noreply.github.com>
2022-04-28 05:17:54 -0400

    Add files via upload

---
+++ ClassPract5.php
@@ -0,0 +1,154 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-216045757-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-216045757-1');
+    </script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content=" Laguna Brava Ecotourism is a local adventure company based in Laguna Brava." />
+    <title>Welcome To Melmaruvathur Adhiparasakthi Worship Center of San Francisco</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+</head>
+
+<body>
+    <header>
+        <!-- Content > images: responsive images 
+                img-fluid maintains a 100% max-width regardless of the size of the screen -->
+        <img src="Karuamma.jpeg" class="img-fluid" alt="Karuvarai Amma image" title="Karuvarai Amma" >
+
+        <!-- Layout > containers
+                Class containers:
+                    container: sets a max-width at each screen size (responsive breakpoint)
+                    container-lg, container-xl, ...: specifies a max-width depending on the size of the screen and 100% max-width until a breakpoint
+                        The result is that for given sizes, the content preserves its margins and centered layout
+                    container-fluid: maintains a 100% max-width regardless of the size of the screen
+                        The result is that content always occupies the entire width of the screen 
+            -->
+        <div class="container">
+            <!-- Content > typography: display headings -->
+            <!-- Utilities > text: text alignment
+                    Options for text alignment: text-start, text-end, text-center, ...
+                -->
+            <h1 class="display-2 text-center" style="color:green;">Welcome To Melmaruvathur Adhiparasakthi Siddhar Worship Center of San Francisco</h1>
+
+            <!-- Content > typography: lead -->
+            <p class="lead" style="color:green;">
+            
+                MASWCOSF is a <a href="https://www.omsakthiamma.org/activities/worshipping_centres">Worship Center</a> of incamparable beauty
+                located in Santa Clara. MASWCOSF is located in a highly developed region.
+                Group of Adhiparasakthi devotees conduct mass prayer on every week,
+                Spiritual Guru Arul Thiru Bnagaru Adihalar Birthday,Amma Vasai, 
+                Pournamimi, Adipouram, Sithra Pournami and Thai Poosam.
+                Spread the message of Incarnation of Mother Godess Sri Adhiparasakthi 
+                and teachings of Spiritual Guru. Arul Thiru Bangaru Adigalar.Through worship center, 
+                we do service for the poor people.                                           
+            </p>
+        </div>
+    </header>
+
+    <main class="container">
+        <!-- Utilities > spacing
+                Options for specifying maring and padding: 
+                    y: vertical, x: horizontal, t: top, b: bottom, s: start, e: end, ... -->
+        <section class="my-4" id="communities">
+            <h2 class="display-5" style="color:green;">The Specialitiy of Worship Center</h2>
+            <p style="color:green;">
+                No To Priests.One of the unique features of this Siddhar Peedam is that 
+                it does not have appointed priests to perform or conduct daily prayers
+                unlike in other temples. The devotees (including women) are allowed 
+                to perform Archanas and Abishekams themselves, in accordance with the procedures
+                laid by the Mother Adhiparasakthi through Her Oracles. 
+                Every day, each one of the odd Worshipping Centres (Mandrams in Tamil) is given 
+                the noble task of serving at the Sanctum Sanctorum.
+                Doing service for the poor people and donate food, dresses, and provide funds
+                for the students for their studies. Our devotees, <strong> donate blood for the health care center 
+                and conducting eye camps for the patients.
+            </p>
+        </section>
+
+        <!-- Layout > grid  -->
+        <section class="my-4 row" id="activities">
+            <h2 class="display-5" style="color:green;">Activities</h2>
+            <p style="color:green;">
+                It is good to wear red color dress to participate in this prayer. In both options, <strong class="text-decoration-underline fw-normal">Reading Manthras, Meditation, Cultural Programs, and Distributing Prasathams.Poor feeling, supplying clothing and industrial equipments to down trodden, free marriages to poor, help to widows, giving aid, wheel chairs, tricycles to disabled etc. are being done on important festivals towards socio economic service to the community.</strong>
+            </p>
+
+            <!-- Utilities > border
+                    Options for specifying border: 
+                        t: top, b: bottom, s: start, e: end, ... 
+                    
+                    For example:
+                    <article class="col-lg px-5 mb-4 border border-secondary border-2"> -->
+            <article class="col-lg px-5 mb-4">
+                <h3 class="display-6 text-center" style="color:green;">About Prayer</h3>
+                <!-- Utilities > border
+                        Options for specifying border that can be used with any element, including images:
+                            rounded, rounded-top, rounded-circle, rounded-pill... -->
+                <figure>
+                    <img src="krAmma2.jpeg" class="img-fluid rounded-pill mb-2 border-2 border-dark" alt="The Karuvarai amma image" title="Karuvarai Amma">
+                    <figcaption style="color:green;">
+                        <!-- Utilities > colors
+                            Other options for specifying colors: background color: bg-warning, ... -->
+
+                        <!--Bootstrap Icons: https://icons.getbootstrap.com/ 
+                                Install -> https://icons.getbootstrap.com/#install -->
+                        We start pooja around 6:00 pm.Tamil Mandras, velvis and kalasa, vilakku Poojas are conducted periodically. <strong class="text-warning fw-bold"><i class="bi bi-exclamation-triangle"></i> This guided pooja is truly trusting, and we recommend it for only those interested devotees.</strong>
+                    </figcaption>
+                </figure>
+
+                <!-- Content > typography: lists -->
+                <ul class="list-unstyled" style="color:green;">
+                    <!-- Utilities > text: font weight -->
+                    <li><span class="fw-bold">Duration: </span>2 hours approx.</li>
+                    <li><span class="fw-bold">Provide: </span>Prasatham</li>
+                    <li><span class="fw-bold">Minimum: </span>10 people</li>
+                </ul>
+                <!-- Components > buttons -->
+                <div class="text-center">
+                    <a class="btn btn-primary btn-sm" href="ClassPract.php">Book here</a>
+                </div>
+            </article>
+
+            <article class="col-lg px-5 mb-4">
+                <h3 class="display-6 text-center" style="color:green;">For Abhisheham</h3>
+                <figure>
+                    <img src="krAmm3.jpeg" class="img-fluid rounded-pill mb-2 border-2 border-dark" alt="Sit Back & Relax boat image" title="Boat trip" float="center">
+                    <figcaption style="color:green;">
+                        Doing Abisheham at the Worship Center:<br> 
+                        .Read Manthras.<br>
+                        .We do Sangalpam for the abishehatharar, and distributing prasatham.
+                        .Children's program. 
+                    </figcaption>
+                </figure>
+                <ul class="list-unstyled" style="color:green;">
+                    <li><span class="fw-bold">Duration: </span>2 hours approx.</li>
+                    <li><span class="fw-bold">Price: </span>$250 USD</li>
+                    <li><span class="fw-bold">Minimum: </span>20 people</li>
+                </ul>
+                <div class="text-center">
+                    <a class="btn btn-primary btn-sm" href="ClassPract.php">Book here</a>
+                </div>
+            </article>
+
+        </section>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
